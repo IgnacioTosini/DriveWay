cee7cfbf64c90a296688015739c0dce6c5b67617
IgnacioMuzio <ignaciomuzio1115@gmail.com>
2023-07-07 15:22:29 -0300

    Posibles cambios finales
    Co-authored-by: Ignacio Tosini <IgnacioTosini@users.noreply.github.com>

--- js/preguntas.js
+++ js/preguntas.js
@@ -1,6 +1,16 @@
 const questions = document.querySelectorAll('.question');
+
 questions.forEach(question => {
-question.addEventListener('click', () => {
-question.classList.toggle('active');
-});
+  question.addEventListener('click', () => {
+    // Comprueba si la pregunta ya está activa
+    const isActive = question.classList.contains('active');
+
+    // Cierra todas las preguntas abiertas
+    questions.forEach(q => q.classList.remove('active'));
+
+    // Abre la pregunta si no estaba previamente activa
+    if (!isActive) {
+      question.classList.add('active');
+    }
+  });
 });
