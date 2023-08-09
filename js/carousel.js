23819aec97926ae63334e7ee9a64b5c492b5286c
ignacio tosini <ignaciotosini2002@gmail.com>
2023-08-09 20:33:25 -0300

    limpieza general. Falta Seo mas alts.

--- js/carousel.js
+++
@@ -1,6 +0,0 @@
-new Glide('.glide', {
-    type: 'carousel',
-    perView: 1,
-    autoplay: 2000, // Intervalo de tiempo en milisegundos para cambiar las diapositivas automáticamente
-    hoverpause: true, // Pausar el carrusel cuando el mouse esté sobre él
-  }).mount();
